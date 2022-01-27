618801545fdd98eb9350e7087f0c8381d546768d
Natã Elienai <nata.elienai@gmail.com>
2022-01-27 18:03:17 -0300

    Cria a tela de explorar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Explore from './pages/Explore';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/explore">
+        <Explore />
+      </Route>
+    </Switch>
   );
 }
 
