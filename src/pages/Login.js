9b79632dc6936fcedc278ce2a9f44672ed9dbd99
Duskim <duskimm@gmail.com>
2022-01-27 19:02:43 -0300

    O req 5 botao enter habilita/desabilitao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import LoginContext from '../context/loginCont/LoginContext';
 
 function Login() {
-  const { email, setEmail, password, setPassword } = useContext(LoginContext);
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    btn,
+    setBtn,
+  } = useContext(LoginContext);
+
+  useEffect(() => {
+    const regex = {
+      email: /^[\w]+([.|\-|_][A-Za-z0-9]+)*@[a-z]{2,}(\.[a-z]{2,})+$/g,
+      rAfterAt: /@[a-z]{2,}(\.[a-z]{2,})+$/g,
+      rAfterAtDots: /(\.[a-z]{2,})+$/g };
+    const minCharacters = 6;
+    if (email.match(regex.email) && password.length > minCharacters) {
+      setBtn({ disabled: false });
+    } else {
+      setBtn({ disabled: true });
+    }
+  }, [email, password, setBtn]);
+
   return (
     <div>
       <form>
@@ -31,6 +52,7 @@ function Login() {
           type="submit"
           name="login_submit_btn"
           data-testid="login-submit-btn"
+          disabled={ btn.disabled }
         >
           Enter
         </button>
