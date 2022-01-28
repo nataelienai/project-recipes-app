2cf231deb8602bc944d3af89fb04eb0734ba62f2
Natã Elienai <nata.elienai@gmail.com>
2022-01-28 16:51:20 -0300

    cria botoes de redirecionamento em Explore Foods e Explore Drinks

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { getRandomDrinkApi } from '../services/api';
 
 export default function ExploreDrinks() {
+  const history = useHistory();
+
+  const handleSurpriseDrink = async () => {
+    const { idDrink } = await getRandomDrinkApi();
+    history.push(`/drinks/${idDrink}`);
+  };
+
   return (
     <div>
-      <button type="button" data-testid="explore-by-ingredient">By Ingridient</button>
-      <button type="button" data-testid="explore-surprise">Surprise me!</button>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/drinks/ingredients') }
+      >
+        By Ingridient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ handleSurpriseDrink }
+      >
+        Surprise me!
+      </button>
     </div>
   );
 }
