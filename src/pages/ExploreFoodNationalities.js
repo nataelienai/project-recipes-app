1c1c2adba4d340a5822887702482617ff32294ae
Natã Elienai <nata.elienai@gmail.com>
2022-01-31 12:12:23 -0300

    cria a pagina de explorar comidas por nacionalidade

---
+++ src/pages/ExploreFoodNationalities.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+export default function ExploreFoodNationalities() {
+  return (
+    <div>
+      <Header title="Explore Nationalities" />
+      <Footer />
+    </div>
+  );
+}
