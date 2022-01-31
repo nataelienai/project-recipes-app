fce9201ad19f412b2a92e65d619be25e5e07a69d
Bruno Alves Fay <brunofay1@hotmail.com>
2022-01-31 01:02:01 -0300

    32 em andamento

--- src/components/MainCards.js
+++ src/components/MainCards.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import HeaderContext from '../context/header/HeaderContext';
+/* import '../Styles/MainCard.css'; */
 
 const MAX_CARDS = 12;
 export default function MainCards() {
@@ -8,10 +10,24 @@ export default function MainCards() {
     dataApi,
     handleMainCardsApi,
   } = useContext(HeaderContext);
+  const history = useHistory();
+  function redirectCards(id) {
+    if (pageDrinkOrFood === 'Food')history.push(`/foods/${id}`);
+    else { history.push(`/drinks/${id}`); }
+  }
 
   function cards(data) {
     return data.map((item, i) => i < MAX_CARDS && (
-      <div key={ i } data-testid={ `${i}-recipe-card` }>
+      <div
+        key={ i }
+        data-testid={ `${i}-recipe-card` }
+      /*   onKeyDown={
+          () => redirectCards(pageDrinkOrFood === 'Food' ? item.idMeal : item.idDrink)
+        }
+        onClick={
+          () => redirectCards(pageDrinkOrFood === 'Food' ? item.idMeal : item.idDrink)
+        } */
+      >
         <img
           data-testid={ `${i}-card-img` }
           alt="card"
@@ -27,6 +43,7 @@ export default function MainCards() {
 
   useEffect(() => {
     handleMainCardsApi();
+    redirectCards('i');
   }, []);
   return (
     <section>
