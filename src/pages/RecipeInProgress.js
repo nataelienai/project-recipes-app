f4e9946540fc23d18c3361e0ab514ec9774632f0
Natã Elienai <nata.elienai@gmail.com>
2022-02-02 16:06:55 -0300

    adiciona todos os itens necessarios na tela de receita em progresso

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -9,7 +9,27 @@ export default function RecipeInProgress() {
   const recipe = useRecipeDetailsById(recipeId);
   const ingredients = useIngredientsFromRecipe(recipe);
 
-  return (
-    <IngredientList ingredients={ ingredients } />
-  );
+  function renderRecipe() {
+    const recipeName = recipe.strMeal || recipe.strDrink;
+    const recipeImgSource = recipe.strMealThumb || recipe.strDrinkThumb;
+
+    return (
+      <div>
+        <img
+          src={ recipeImgSource }
+          alt={ recipeName }
+          data-testid="recipe-photo"
+        />
+        <h1 data-testid="recipe-title">{recipeName}</h1>
+        <button type="button" data-testid="share-btn">Share</button>
+        <button type="button" data-testid="favorite-btn">Favorite</button>
+        <h2 data-testid="recipe-category">{recipe.strCategory}</h2>
+        <IngredientList ingredients={ ingredients } />
+        <p data-testid="instructions">{recipe.strInstructions}</p>
+        <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
+      </div>
+    );
+  }
+
+  return recipe ? renderRecipe() : null;
 }
