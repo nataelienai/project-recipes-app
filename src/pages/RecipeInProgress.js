684335e3b922a90fa3332f29735898ce955a0781
Natã Elienai <nata.elienai@gmail.com>
2022-02-02 20:10:00 -0300

    cria botao para compartilhar a receita

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import IngredientList from '../components/IngredientList';
+import ShareButton from '../components/ShareButton';
 import useIngredientsFromRecipe from '../hooks/useIngredientsFromRecipe';
 import useRecipeDetailsById from '../hooks/useRecipeDetailsById';
 
@@ -8,24 +9,43 @@ export default function RecipeInProgress() {
   const { recipeId } = useParams();
   const recipe = useRecipeDetailsById(recipeId);
   const ingredients = useIngredientsFromRecipe(recipe);
+  const { pathname } = useLocation();
+
+  function getRecipeDetails() {
+    const isDrink = pathname.startsWith('/drinks');
+
+    const recipeDetails = {
+      name: recipe.strMeal,
+      imgSource: recipe.strMealThumb,
+      category: recipe.strCategory,
+      instructions: recipe.strInstructions,
+    };
+
+    if (isDrink) {
+      recipeDetails.name = recipe.strDrink;
+      recipeDetails.imgSource = recipe.strDrinkThumb;
+      recipeDetails.category = recipe.strAlcoholic;
+    }
+
+    return recipeDetails;
+  }
 
   function renderRecipe() {
-    const recipeName = recipe.strMeal || recipe.strDrink;
-    const recipeImgSource = recipe.strMealThumb || recipe.strDrinkThumb;
+    const { name, imgSource, category, instructions } = getRecipeDetails();
 
     return (
       <div>
         <img
-          src={ recipeImgSource }
-          alt={ recipeName }
+          src={ imgSource }
+          alt={ name }
           data-testid="recipe-photo"
         />
-        <h1 data-testid="recipe-title">{recipeName}</h1>
-        <button type="button" data-testid="share-btn">Share</button>
+        <h1 data-testid="recipe-title">{name}</h1>
         <button type="button" data-testid="favorite-btn">Favorite</button>
-        <h2 data-testid="recipe-category">{recipe.strCategory}</h2>
+        <ShareButton />
+        <h2 data-testid="recipe-category">{category}</h2>
         <IngredientList ingredients={ ingredients } />
-        <p data-testid="instructions">{recipe.strInstructions}</p>
+        <p data-testid="instructions">{instructions}</p>
         <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
       </div>
     );
