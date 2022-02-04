48d8a99ac5f8d9bdd4658e24d7ea525458d73e51
ronnymv <rjmv.dev@gmail.com>
2022-02-04 17:19:01 -0300

    feat(req60/61): implement fav recipes page

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -2,10 +2,15 @@ import React, { useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import HeaderContext from '../context/header/HeaderContext';
 import Header from '../components/Header';
+import FiltersButtonsDoneRecipe from '../components/FiltersButtonsDoneRecipe';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 export default function FavoriteRecipes() {
   const location = useLocation();
   const { setSearchButton } = useContext(HeaderContext);
+  const getFavoriteFoodsLs = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
   useEffect(() => {
     if (location.pathname === '/favorite-recipes') setSearchButton(false);
   }, []);
@@ -13,6 +18,37 @@ export default function FavoriteRecipes() {
   return (
     <div>
       <Header title="Favorite Recipes" />
+      <FiltersButtonsDoneRecipe />
+
+      {getFavoriteFoodsLs.map((food, index) => (
+        <div key={ food.id }>
+          <button type="button">
+            <img
+              alt="fav-bttn"
+              src={ blackHeartIcon }
+              data-testid={ `${index}-horizontal-favorite-btn` }
+            />
+          </button>
+          <button
+            type="button"
+          >
+            <img
+              alt="share-bttn"
+              src={ shareIcon }
+              data-testid={ `${index}-horizontal-share-btn` }
+            />
+          </button>
+          <img
+            alt="food-fav"
+            data-testid={ `${index}-horizontal-image` }
+            src={ food.image }
+          />
+          <h1 data-testid={ `${index}-horizontal-name` }>{food.name}</h1>
+          <p>{food.category}</p>
+          <p data-testid={ `${index}-horizontal-top-text` }>{food.nationality}</p>
+
+        </div>
+      ))}
 
     </div>
   );
