419b087fa05f78534968b052fa76ae58b393edd4
Natã Elienai <nata.elienai@gmail.com>
2022-02-11 12:36:10 -0300

    implementa os testes do requisito 71

--- src/test/05-ExploreRecipes.test.js
+++ src/test/05-ExploreRecipes.test.js
@@ -4,14 +4,18 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import '@testing-library/jest-dom';
 
+const EXPLORE_BY_INGREDIENT_TEST_ID = 'explore-by-ingredient';
+const EXPLORE_BY_NATIONALITY_TEST_ID = 'explore-by-nationality';
+const SURPRISE_ME_TEST_ID = 'explore-surprise';
+
 describe('Explore Foods e Explore Drinks', () => {
   describe('70 - Implemente os elementos da tela de explorar bebidas ou comidas', () => {
     it('Tem os data-testids corretos para a tela de explorar comidas', () => {
       renderWithRouter(<App />, { route: '/explore/foods' });
 
-      const exploreByIngredientBtn = screen.getByTestId('explore-by-ingredient');
-      const exploreByNationalityBtn = screen.getByTestId('explore-by-nationality');
-      const surpriseMeBtn = screen.getByTestId('explore-surprise');
+      const exploreByIngredientBtn = screen.getByTestId(EXPLORE_BY_INGREDIENT_TEST_ID);
+      const exploreByNationalityBtn = screen.getByTestId(EXPLORE_BY_NATIONALITY_TEST_ID);
+      const surpriseMeBtn = screen.getByTestId(SURPRISE_ME_TEST_ID);
 
       expect(exploreByIngredientBtn).toBeInTheDocument();
       expect(exploreByNationalityBtn).toBeInTheDocument();
@@ -21,9 +25,11 @@ describe('Explore Foods e Explore Drinks', () => {
     it('Tem os data-testids corretos para a tela de explorar bebidas', () => {
       renderWithRouter(<App />, { route: '/explore/drinks' });
 
-      const exploreByIngredientBtn = screen.getByTestId('explore-by-ingredient');
-      const exploreByNationalityBtn = screen.queryByTestId('explore-by-nationality');
-      const surpriseMeBtn = screen.getByTestId('explore-surprise');
+      const exploreByIngredientBtn = screen.getByTestId(EXPLORE_BY_INGREDIENT_TEST_ID);
+      const exploreByNationalityBtn = screen.queryByTestId(
+        EXPLORE_BY_NATIONALITY_TEST_ID,
+      );
+      const surpriseMeBtn = screen.getByTestId(SURPRISE_ME_TEST_ID);
 
       expect(exploreByIngredientBtn).toBeInTheDocument();
       expect(exploreByNationalityBtn).not.toBeInTheDocument();
@@ -33,10 +39,35 @@ describe('Explore Foods e Explore Drinks', () => {
 
   describe('71 - Botões de explorar por ingrediente, nacionalidade e receita aleatória',
     () => {
-      it.todo(
-        'Explore Foods tem os botões "By Ingredient", "By Nationality" e "Suprise me!"',
-      );
-      it.todo('Explore Drinks tem os botões "By Ingredient" e "Surprise me!"');
+      it('Explore Foods tem os botões "By Ingredient", "By Nationality" e "Suprise me!"',
+        () => {
+          renderWithRouter(<App />, { route: '/explore/foods' });
+
+          const exploreByIngredientBtn = screen.getByTestId(
+            EXPLORE_BY_INGREDIENT_TEST_ID,
+          );
+          const exploreByNationalityBtn = screen.getByTestId(
+            EXPLORE_BY_NATIONALITY_TEST_ID,
+          );
+          const surpriseMeBtn = screen.getByTestId(SURPRISE_ME_TEST_ID);
+
+          expect(exploreByIngredientBtn).toHaveTextContent('By Ingredient');
+          expect(exploreByNationalityBtn).toHaveTextContent('By Nationality');
+          expect(surpriseMeBtn).toHaveTextContent('Surprise me!');
+        });
+      it('Explore Drinks tem os botões "By Ingredient" e "Surprise me!"', () => {
+        renderWithRouter(<App />, { route: '/explore/drinks' });
+
+        const exploreByIngredientBtn = screen.getByTestId(EXPLORE_BY_INGREDIENT_TEST_ID);
+        const exploreByNationalityBtn = screen.queryByTestId(
+          EXPLORE_BY_NATIONALITY_TEST_ID,
+        );
+        const surpriseMeBtn = screen.getByTestId(SURPRISE_ME_TEST_ID);
+
+        expect(exploreByIngredientBtn).toHaveTextContent('By Ingredient');
+        expect(exploreByNationalityBtn).not.toBeInTheDocument();
+        expect(surpriseMeBtn).toHaveTextContent('Surprise me!');
+      });
     });
 
   describe('72 - Ao clicar em "By Ingredient", explora receita por ingrediente', () => {
