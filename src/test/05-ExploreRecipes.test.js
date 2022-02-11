a5b1a5b1f256401f97d7c7f9af4dff1da1401ff7
Natã Elienai <nata.elienai@gmail.com>
2022-02-11 12:20:56 -0300

    troca variaveis 'test' por 'it'

--- src/test/05-ExploreRecipes.test.js
+++ src/test/05-ExploreRecipes.test.js
@@ -6,7 +6,7 @@ import '@testing-library/jest-dom';
 
 describe('Explore Foods e Explore Drinks', () => {
   describe('70 - Implemente os elementos da tela de explorar bebidas ou comidas', () => {
-    test('Tem os data-testids corretos para a tela de explorar comidas', () => {
+    it('Tem os data-testids corretos para a tela de explorar comidas', () => {
       renderWithRouter(<App />, { route: '/explore/foods' });
 
       const exploreByIngredientBtn = screen.getByTestId('explore-by-ingredient');
@@ -18,7 +18,7 @@ describe('Explore Foods e Explore Drinks', () => {
       expect(surpriseMeBtn).toBeInTheDocument();
     });
 
-    test('Tem os data-testids corretos para a tela de explorar bebidas', () => {
+    it('Tem os data-testids corretos para a tela de explorar bebidas', () => {
       renderWithRouter(<App />, { route: '/explore/drinks' });
 
       const exploreByIngredientBtn = screen.getByTestId('explore-by-ingredient');
@@ -33,23 +33,23 @@ describe('Explore Foods e Explore Drinks', () => {
 
   describe('71 - Botões de explorar por ingrediente, nacionalidade e receita aleatória',
     () => {
-      test.todo(
+      it.todo(
         'Explore Foods tem os botões "By Ingredient", "By Nationality" e "Suprise me!"',
       );
-      test.todo('Explore Drinks tem os botões "By Ingredient" e "Surprise me!"');
+      it.todo('Explore Drinks tem os botões "By Ingredient" e "Surprise me!"');
     });
 
   describe('72 - Ao clicar em "By Ingredient", explora receita por ingrediente', () => {
-    test.todo('Ao clicar em "By Ingredient", explora comidas por ingrediente');
-    test.todo('Ao clicar em "By Ingredient", explora bebidas por ingrediente');
+    it.todo('Ao clicar em "By Ingredient", explora comidas por ingrediente');
+    it.todo('Ao clicar em "By Ingredient", explora bebidas por ingrediente');
   });
 
   describe('73 - Ao clicar em "By Nationality", explora comida por nacionalidade', () => {
-    test.todo('A rota deve mudar para tela de explorar por nacionalidades');
+    it.todo('A rota deve mudar para tela de explorar por nacionalidades');
   });
 
   describe('74 - Ao clicar em "Surprise me!", vai para uma receita aleatória', () => {
-    test.todo('Ao clicar em "Surprise me!", vai para uma comida aleatória');
-    test.todo('Ao clicar em "Surprise me!", vai para uma bebida aleatória');
+    it.todo('Ao clicar em "Surprise me!", vai para uma comida aleatória');
+    it.todo('Ao clicar em "Surprise me!", vai para uma bebida aleatória');
   });
 });
